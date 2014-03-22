1ffb78926fdf8a101ddd659e99e22eb52d133192
shahar zeira <shahar.zeira@gmail.com>
2014-03-22 12:24:43 +0200

    Update saveToMp3.py

--- saveToMp3.py
+++ saveToMp3.py
@@ -3,10 +3,10 @@ import os
 
 def save(mp4):
     a=["ffmpeg -i","'"+getFull(mp4)+"'","-q:a 0 -map a", "'"+getFullFromMp4(mp4)+"'"]
-    printData(mp4, a)
+    printData(mp4)
     os.system(" ".join(a))
 
-def printData(mp4, a):
+def printData(mp4):
     print "******************"
     print getFull(mp4)
     print getFullFromMp4(mp4)
