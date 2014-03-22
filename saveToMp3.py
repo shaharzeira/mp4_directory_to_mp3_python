15e11fe853a173b18beec16c9de8111caef52fc1
shahar zeira <shahar.zeira@gmail.com>
2014-03-22 11:51:01 +0200

    Create saveToMp3.py

---
+++ saveToMp3.py
@@ -0,0 +1,15 @@
+from parser import getFull, getFullFromMp4
+from subprocess import call, Popen
+import os
+
+def save(mp4):
+    a=["ffmpeg -i","'"+getFull(mp4)+"'","-q:a 0 -map a", "'"+getFullFromMp4(mp4)+"'"]
+    printData(mp4, a)
+    os.system(" ".join(a))
+
+def printData(mp4, a):
+    print "******************"
+    print getFull(mp4)
+    print getFullFromMp4(mp4)
+    print "******************"
+    print " ".join(a)
