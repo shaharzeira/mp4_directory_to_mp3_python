d71703783419f15927e1e83afb8baed47bdf63fc
shahar zeira <shahar.zeira@gmail.com>
2014-04-04 11:57:42 +0300

    Update findAllMp4Files.py

--- findAllMp4Files.py
+++ findAllMp4Files.py
@@ -23,7 +23,6 @@ def findFiles(dirFiles):
     print dirFiles
     a=[]
     for fileMp4 in listdir(dirFiles):
-	if fileMp4.endswith(".mp4"):
-	    print fileMp4
-            a.append(parse(fileMp4, dirFiles))
+	print fileMp4
+        a.append(parse(fileMp4, dirFiles))
     return a
